a8581100c2942be9371876d62ac2feb3c92d6ff2
NLPxiaoxu <46883256+NLPxiaoxu@users.noreply.github.com>
2019-01-24 09:28:30 +0800

    Update predict_cnn.py

--- TextCnn/predict_cnn.py
+++ TextCnn/predict_cnn.py
@@ -1,45 +1,8 @@
 #encoding:utf-8
-import os
-import jieba
-import random
 import numpy as np
 from Text_Cnn import *
-from data_processing import read_category, get_wordid, get_word2vec, process
+from data_processing import read_category, get_wordid, get_word2vec, process，batch_iter, sequence
 from Parameters import Parameters as pm
-
-
-def read_file(filename):
-
-    re_han = re.compile(u"([\u4E00-\u9FD5a-zA-Z0-9+#&\._%]+)")  # the method of cutting text by punctuation
-    contents = []
-    with codecs.open(filename, 'r', encoding='utf-8') as f:
-        for line in f:
-            try:
-                line = line.rstrip()
-                assert len(line.split('\t')) == 2
-                _, content = line.split('\t')
-                blocks = re_han.split(content)
-                word = []
-                for blk in blocks:
-                    if re_han.match(blk):
-                        word.extend(jieba.lcut(blk))
-                contents.append(word)
-            except:
-                pass
-    return contents
-
-
-def Process(sentences, word_to_id, max_length=600):
-
-
-    data_id = []
-    for i in range(len(sentences)):
-        data_id.append([word_to_id[x] for x in sentences[i] if x in word_to_id])
-
-    x_pad = kr.preprocessing.sequence.pad_sequences(data_id, max_length, padding='post', truncating='post')
-
-    return x_pad
-
 
 def val():
     pre_label = []
